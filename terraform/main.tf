3ca54382b7e23f53f39a7809d111e73c5ad163ac
Nikita Kemarskiy <nikitakemarskiy@gmail.com>
2023-05-30 08:17:20 +0300

    Add IaC using Terraform

---
+++ terraform/main.tf
@@ -0,0 +1,66 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.6"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+# Ubuntu AMI
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+# Security Group
+resource "aws_security_group" "lab-web-app-security-group" {
+  name        = "lab-web-app-security-group"
+  description = "Allow HTTP and SSH access"
+
+  # Inbound rules
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 instance for application
+resource "aws_instance" "lab-web-app" {
+  instance_type          = "t2.micro"
+  ami                    = data.aws_ami.ubuntu.id
+  vpc_security_group_ids = [aws_security_group.lab-web-app-security-group.id]
+
+  # Run init shell script
+  user_data = file("web-app-init-script.sh")
+
+  tags = {
+    Name = "lab-resources"
+  }
+}
---
+++ terraform/web-app-init-script.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+IMAGE_NAME=savstanis/infrastructure-project
+
+if [ -z "$IMAGE_NAME" ]; then
+    echo "The IMAGE_NAME environment variable is not set."
+    exit 1
+fi
+
+if ! command -v docker &> /dev/null; then
+    echo "Docker is not installed. Installing Docker..."
+    
+    # Update the package index
+    sudo apt update
+    
+    # Install necessary packages to use HTTPS
+    sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+    
+    # Add the Docker GPG key
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    
+    # Add the Docker repository
+    echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    
+    # Update the package index again
+    sudo apt update
+    
+    # Install Docker
+    sudo apt install -y docker-ce docker-ce-cli containerd.io
+    
+    # Add the current user to the 'docker' group to run Docker without sudo
+    sudo usermod -aG docker $USER
+    
+    echo "Docker has been installed successfully."
+else
+    echo "Docker is already installed."
+fi
+
+docker run --detach "$IMAGE_NAME"
+
+docker run --detach \
+    --name watchtower \
+    --volume /var/run/docker.sock:/var/run/docker.sock \
+    containrrr/watchtower
+
